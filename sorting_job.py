4d021c1d041f3e7fd2816a4659a09ce837a80187
Reggie <reggieandrade79@gmail.com>
2025-03-24 18:23:36 -0400

    Update sorting_job

--- sorting_job.py
+++ sorting_job.py
@@ -11,23 +11,25 @@ def createLogFile(log_dir="logs"):
     log_path = os.path.join(log_dir, f"sorting_log_{timestamp}.txt")
     return open(log_path, "a")  # Caller is responsible for closing log file
 
+# Helper function to flatten a folder structure
+def getAllFiles(folder):
+    for item in folder.contents:
+        if isinstance(item, FileInfo):
+            yield item
+        elif isinstance(item, FolderInfo):
+            yield from getAllFiles(item)
 
 def runSortingJob(rulesets, target_folder, log_dir="logs"):
     if not isinstance(target_folder, FolderInfo):
         raise ValueError("Target folder must be a valid FolderInfo object.")
 
     log_file = createLogFile(log_dir)
+    all_files = list(getAllFiles(target_folder))
 
     try:
-        for filename in os.listdir(target_folder):
-            file_path = os.path.join(target_folder, filename)
+        for file in all_files:
+            for ruleset in rulesets:
+                ruleset.runRules(file, logger=log_file)
 
-            if os.path.isfile(file_path):
-                file_info = FileInfo.fromPath(file_path)
-
-                for ruleset in rulesets:
-                    ruleset.runRules(file_info, logger=log_file)
     finally:
         log_file.close()
-
-# TODO: Finish logging functionality, edit action.py accordingly
