249574b9f888fb0966eddae1c03efbecadf42216
Reggie <reggieandrade79@gmail.com>
2025-03-25 00:39:36 -0400

    Add main testing

--- sorting_job.py
+++ sorting_job.py
@@ -3,6 +3,11 @@ from datetime import datetime
 from file_info import FileInfo
 from folder_info import FolderInfo
 
+from sorting_rule import SortingRule
+from ruleset import Ruleset
+from condition import Condition
+from action import Action
+
 def createLogFile(log_dir="logs"):
     if not os.path.exists(log_dir):
         os.makedirs(log_dir)
@@ -33,3 +38,31 @@ def runSortingJob(rulesets, target_folder, log_dir="logs"):
 
     finally:
         log_file.close()
+
+def main():
+    # Define the target directory
+    target_directory = r"C:\Users\Reggie\Files\Documents\Code\Python\QwikSortTestFolders\perf_test_data - Copy"
+    target = FolderInfo.fromPath(target_directory, True)
+
+    # Create some rulesets
+
+    photosAction = Action("move", os.path.join(target_directory, "Photos"))
+    photosRuleset = Ruleset.fromRules(target, [ 
+        SortingRule(Condition("extension", "==", ".png"), photosAction),
+        SortingRule(Condition("extension", "==", ".jpg"), photosAction),
+        SortingRule(Condition("name", "contains", "photo"), photosAction)
+    ])
+
+    videosAction = Action("move", os.path.join(target_directory, "Videos"))
+    videosRuleset = Ruleset.fromRules(target, [ 
+        SortingRule(Condition("extension", "==", ".mp4"), videosAction),
+        SortingRule(Condition("name", "contains", "video"), videosAction)
+    ])
+
+    rulesets = [photosRuleset, videosRuleset]
+
+    # Run the job with the created rulesets on the target folder
+    runSortingJob(rulesets, target)
+
+if __name__ == "__main__":
+    main()
