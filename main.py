7a44bd38fb42506a2642db13faf5ddd67bceb1a4
omegadc <dcleader99@gmail.com>
2025-04-04 18:39:32 -0400

    Created Error Message template and created functionality for back/forward button

--- main.py
+++ main.py
@@ -2,11 +2,13 @@ import sys
 import os
 from pathlib import Path
 from PySide6.QtWidgets import QApplication, QWidget, QHBoxLayout, QMainWindow, QDialog, QFileSystemModel, QFileDialog, QLabel
-from PySide6.QtWidgets import ( QCheckBox, QTreeWidgetItem, QTreeWidget, QDateTimeEdit, QCalendarWidget, QListWidgetItem )
+from PySide6.QtWidgets import ( QLineEdit, QStyleFactory, QCheckBox, QTreeWidgetItem, QTreeWidget, QDateTimeEdit, QCalendarWidget, QListWidgetItem )
 from PySide6.QtCore import QDir, QModelIndex, Qt, QCalendar
+from PySide6.QtGui import QPalette, QColor
 # Front End Import
 from Frontend.MainWindow import Ui_MainWindow
 from Frontend.ruleset import Ui_Dialog
+from Frontend.ErrorMessageFolder import ErrorDialog
 # Backend Import
 # from Backend.action import *
 # from Backend.sorting_job import *
@@ -31,6 +33,14 @@ def create_item_widget(text, control_widget):
     # Optionally add a stretch if you want the widget aligned to the left
     layout.addStretch()
     return widget
+
+class ErrorMessage(QDialog):
+    def __init__(self):
+        super().__init__()
+        self.ui = ErrorDialog()
+        self.ui.setupUi(self)
+        self.ui.buttonBox.setCenterButtons(True)
+        self.ui.buttonBox.rejected.connect(self.reject)
 
 class RulesetWindow(QDialog):
     def __init__(self):
@@ -67,8 +77,8 @@ class RulesetWindow(QDialog):
         datetime_edit2.setDisplayFormat("yyyy-MM-dd HH:mm:ss")
         widget1 = create_item_widget("Modified", datetime_edit)
         widget2 = create_item_widget("Created", datetime_edit2)
-        datetime_item = QTreeWidgetItem(["Modified"])
-        datetime_item2 = QTreeWidgetItem(["Created"])
+        datetime_item = QTreeWidgetItem()
+        datetime_item2 = QTreeWidgetItem()
         date_item.addChild(datetime_item)
         date_item.addChild(datetime_item2)
         self.ui.listView.setItemWidget(datetime_item, 0, widget1)
@@ -77,11 +87,35 @@ class RulesetWindow(QDialog):
         # Name
         name_item = QTreeWidgetItem(["Name"])
         self.ui.listView.addTopLevelItem(name_item)
+        self.regexInclude = QLineEdit(self)
+        self.regexExclude = QLineEdit(self)
+        self.regexInclude.textChanged.connect(self.filter_files)
+        widget_include = create_item_widget("Include",self.regexInclude)
+        widget_exclude = create_item_widget("Exclude",self.regexExclude)
+        name_item1 = QTreeWidgetItem()
+        name_item2 = QTreeWidgetItem()
+        name_item.addChild(name_item1)
+        name_item.addChild(name_item2)
+        self.ui.listView.setItemWidget(name_item1, 0, widget_include)
+        self.ui.listView.setItemWidget(name_item2, 0, widget_exclude)
+        
+
         # Other 
         other_item = QTreeWidgetItem(["Other"])
         self.ui.listView.addTopLevelItem(other_item)
 
+
         self.ui.listView.expandAll()
+
+    def filter_files(self):
+        keywords = [kw.strip().lower() for kw in self.regexInclude.text().split(",") if kw.strip()]
+        self.ui.listView.clear()
+
+        filtered_files = [
+            file for file in self.ui.listView 
+            if all(kw in file.lower() for kw in keywords)
+        ]
+        self.ui.listView.setItemWidget(filtered_files)
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -89,6 +123,10 @@ class MainWindow(QMainWindow):
         self.ui = Ui_MainWindow()  # Create an instance of the UI class
         self.ui.setupUi(self)  # Set up the UI
         self.ui.actionOpen_Folder.triggered.connect(self.change_directory)
+
+        self.state = None
+        self.filepath = None
+
 
         # Changing file directory
         self.ui.pushbtn_Dir.clicked.connect(self.change_directory)
@@ -115,29 +153,62 @@ class MainWindow(QMainWindow):
         self.ui.pushButton_2.clicked.connect(self.backButtonDir)
         self.ui.pushButton_3.clicked.connect(self.forwardButtonDir)
 
-        # lastDirectory should retain the last directory visited
-        self.lastDirectory = QDir(self.model.filePath(self.ui.listFiles.rootIndex()))
-        self.ui.listFiles.clicked
+        # Clicked Item reveals forwardBttn directory when clicked
+        self.ui.listFiles.clicked.connect(self.oneItemClicked)
+
+        # Light/Dark Mode
+        self.is_dark_mode = True
+        self.set_dark_theme()
+        # self.ui.actionLight_Dark_Mode.triggered.connect(self.toggle_theme)
     
-    def forwardButtonDir(self):
-        lastDir = self.lastDirectory
-        lastDir.canonicalPath
-        self.ui.listFiles.setRootIndex(self.model.index(lastDir.canonicalPath))
-        self.ui.leTargetDirectory.setText(lastDir.canonicalPath)
+    # Light/Dark mode Implementation
+    # def toggle_theme(self):
+    #     if self.is_dark_mode:
+    #         self.set_light_theme()
+    #     else:
+    #         self.set_dark_theme()
+    #     self.is_dark_mode = not self.is_dark_mode
 
-        item = QListWidgetItem()
-        item.c
+    # def set_light_theme(self):
+    #     app.setStyle(QStyleFactory.create("Fusion"))
+    #     palette = QPalette()
+    #     palette.setColor(QPalette.ColorRole.Window, QColor(240, 240, 240))
+    #     palette.setColor(QPalette.ColorRole.WindowText, QColor(0,0,0))
+    #     app.setPalette(palette)
+    
+    def set_dark_theme(self):
+        app.setStyle(QStyleFactory.create("Fusion"))
+        palette = QPalette()
+        palette.setColor(QPalette.ColorRole.Window, QColor(53, 53, 53))
+        palette.setColor(QPalette.ColorRole.WindowText, QColor(255, 255, 255))
+        app.setPalette(palette)
 
+    def oneItemClicked(self, index: QModelIndex):
+        # self.filepath = self.model.filePath(index)
+        if index.isValid():
+            selected_path = self.model.filePath(index)
+            if QDir(selected_path).exists():
+                self.filepath = selected_path
+
+
+    def forwardButtonDir(self, index: QModelIndex):
+        if self.filepath == None:
+            dialog = ErrorMessage()
+            dialog.exec()
+        else:
+            selected_path = self.filepath
+            if QDir(selected_path).exists():
+                os.chdir(selected_path)
+                self.update_directory_view(selected_path)
 
     def backButtonDir(self):
         path_index = self.ui.listFiles.rootIndex()
         path = self.model.filePath(path_index)
         directory = QDir(path)
-        self.lastDirectory = directory
+        self.filepath = path
         if directory.cdUp():
             parent_dir = directory.absolutePath()
-            self.ui.listFiles.setRootIndex(self.model.index(parent_dir))
-            self.ui.leTargetDirectory.setText(parent_dir)
+            self.update_directory_view(parent_dir)
         else:
             print("Already at the top-level Directory")
 
@@ -167,6 +238,7 @@ class MainWindow(QMainWindow):
         dialog.exec()
 
     def on_list_view_click(self, index: QModelIndex):
+        self.filepath = index
         if index.isValid():
             selected_path = self.model.filePath(index)
             if QDir(selected_path).exists():
